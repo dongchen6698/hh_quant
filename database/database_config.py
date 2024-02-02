8d0c92bd39c91c730a43bdefb5516119875e0866
灵庭 <dongchen.cd@alibaba-inc.com>
2024-02-02 11:54:35 +0800

    continue coding database part,add base downloader and uploader

--- database/database_config.py
+++ database/database_config.py
@@ -1,4 +1,10 @@
-# database_config.py
-
-DATABASE_PATH = "./hh_quant_stock.db"
+# 数据库相关配置
+DATABASE_PATH = "./hh_quant.db"
 DATABASE_SCHEMA_PATH = "./database_schema.sql"
+
+# 数据库表映射关系
+TABLE_STOCK_TRADE_DATA_INFO = "hh_quant_stock_trade_date_info"
+TABLE_STOCK_BASE_INFO = "hh_quant_stock_base_info"
+TABLE_STOCK_HISTORY_INFO = "hh_quant_stock_history_info"
+TABLE_STOCK_INDIVIDUAL_INFO = "hh_quant_stock_individual_info"
+TABLE_STOCK_EVENT_INFO = "hh_quant_stock_event_info"
--- database/database_main.py
+++ database/database_main.py
@@ -1,18 +1,31 @@
+import os
 import sqlite3
-import database_config as db_config
-from downloader.akshare_downloader import AkShareDownloader
+import database_config as config
+from database.uploader.uploader_akshare import AkShareUploader
 
 
-def init_databse():
-    # 连接到SQLite数据库
-    # 如果数据库文件不存在，SQLite将会创建一个
-    conn = sqlite3.connect(db_config.DATABASE_PATH)
+# 数据相关配置
+INIT_START_DATE = "20000101"
+INIT_END_DATA = "20050101"
+
+
+def init_database():
+    # 检测数据库状态
+    if not os.path.exists(config.DATABASE_PATH):
+        # 如果数据库文件不存在，SQLite将会创建一个
+        conn = sqlite3.connect(config.DATABASE_PATH)
+        print(f"数据库 {config.DATABASE_PATH} 创建成功。")
+        conn.close()
+
+
+def init_database_schema():
+    conn = sqlite3.connect(config.DATABASE_PATH)
+    print(f"数据库 {config.DATABASE_PATH} 连接成功。")
     # 创建一个cursor对象来帮助执行SQL语句
     cursor = conn.cursor()
     # 读取SQL模式文件
-    with open(db_config.DATABASE_SCHEMA_PATH, "r") as f:
+    with open(config.DATABASE_SCHEMA_PATH, "r") as f:
         schema_sql = f.read()
-    # 使用executescript()执行SQL脚本
     # 如果.sql文件包含多个SQL语句，确保它们以分号结束
     cursor.executescript(schema_sql)
     # 提交事务
@@ -20,28 +33,26 @@ def init_databse():
     # 关闭cursor和连接
     cursor.close()
     conn.close()
-    print(f"数据库和表已经从文件 {db_config.DATABASE_SCHEMA_PATH} 创建于 {db_config.DATABASE_PATH}")
+    print(f"数据库和表已经从文件 {config.DATABASE_SCHEMA_PATH} 创建于 {config.DATABASE_PATH}")
 
 
-def download_data():
-    db_conn = sqlite3.connect(db_config.DATABASE_PATH)
-    start_date = "20000101"
-    end_date = "20050101"
+def init_database_data(start_date, end_date):
+    conn = sqlite3.connect(config.DATABASE_PATH)
     # 初始化downloader
-    downloader = AkShareDownloader(db_conn=db_conn, start_date=start_date, end_date=end_date)
-    downloader._download_stock_trade_date()
-    downloader._download_stock_base_info()
-    downloader._download_stock_individual_info()
-    downloader._download_stock_history_info()
-    db_conn.close()
-
-
-def main():
-    # 初始化数据库
-    init_databse()
-    # 插入数据
-    download_data()
+    uploader = AkShareUploader(db_conn=conn)
+    uploader._upload_stock_trade_date(table_name=config.TABLE_STOCK_TRADE_DATA_INFO)
+    uploader._upload_stock_base_info(table_name=config.TABLE_STOCK_BASE_INFO)
+    uploader._upload_stock_individual_info(table_name=config.TABLE_STOCK_INDIVIDUAL_INFO)
+    uploader._upload_stock_history_info(table_name=config.TABLE_STOCK_HISTORY_INFO, start_date=start_date, end_date=end_date)
+    uploader._upload_stock_event_info(table_name=config.TABLE_STOCK_EVENT_INFO, start_date=start_date, end_data=end_date)
+    # 关闭连接
+    conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    # 初始化数据库
+    init_database()
+    # 初始化数据表
+    init_database_schema()
+    # 插入数据
+    init_database_data(start_date="20000101", end_date="20100101")
