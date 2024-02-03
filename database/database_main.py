f503fe8b13703ba46157b58606b9d7d14108b8bb
dong <dongchen6698@gmail.com>
2024-02-03 10:16:44 +0800

    commit part

--- database/database_main.py
+++ database/database_main.py
@@ -1,13 +1,21 @@
+import sqlite3
 
 
-
-
-
-def main():
-    # 初始化数据库
-
-    # 插入数据
+def init_database(sql_file_path="database_schema.sql"):
+    # 创建连接对象
+    conn = sqlite3.connect("example.db")
+    # 创建游标对象
+    cur = conn.cursor()
+    # 打开SQL文件
+    with open("sql_file_path", "r") as f:
+        sql = f.read()
+    # 执行SQL文件
+    cur.executescript(sql)
+    # 提交更改
+    conn.commit()
+    # 关闭连接
+    conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    init_database()
