cf63d872d6e9ef57c8dad301c7d9afce4f57a440
adongdong <dongchen6698@gmail.com>
2024-06-04 09:34:06 +0000

    finish alpha191 expression engine excutor part, except some error expression

--- Share/database_auto/db_factor_prebuilder/utils/expression_excutor.py
+++ Share/database_auto/db_factor_prebuilder/utils/expression_excutor.py
@@ -5,21 +5,11 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 import numpy as np
 import re
+from scipy.stats import linregress
 
 
 class AlphaBaseOperations:
-    @staticmethod
-    def min(series1, series2):
-        return np.minimum(series1, series2)
-
-    @staticmethod
-    def max(series1, series2):
-        return np.maximum(series1, series2)
-
-    @staticmethod
-    def shift(series, period):
-        return series.shift(period)
-
+    # ------------------------------------------
     @staticmethod
     def rank(series):
         return series.rank(pct=True)
@@ -29,10 +19,6 @@ class AlphaBaseOperations:
         return np.log(series)
 
     @staticmethod
-    def delta(series, period):
-        return series.diff(period)
-
-    @staticmethod
     def sign(series):
         return np.sign(series)
 
@@ -40,14 +26,25 @@ class AlphaBaseOperations:
     def abs(series):
         return np.abs(series)
 
+    # ------------------------------------------
     @staticmethod
-    def correlation(series1, series2, window):
-        return series1.rolling(window=window).corr(series2)
+    def min(series1, series2):
+        return np.minimum(series1, series2)
 
     @staticmethod
-    def covariance(series1, series2, window):
-        return series1.rolling(window=window).cov(series2)
+    def max(series1, series2):
+        return np.maximum(series1, series2)
 
+    # ------------------------------------------
+    @staticmethod
+    def shift(series, periods):
+        return series.shift(periods)
+
+    @staticmethod
+    def diff(series, periods):
+        return series.diff(periods)
+
+    # ------------------------------------------
     @staticmethod
     def std(series, window):
         return series.rolling(window=window).std()
@@ -69,95 +66,6 @@ class AlphaBaseOperations:
         return series.rolling(window=window).min()
 
     @staticmethod
-    def quantile(series, window, quantile):
-        return series.rolling(window=window).quantile(quantile)
-
-    @staticmethod
-    def idxmax(series, window):
-        return series.rolling(window=window).apply(lambda x: (window - 1) - x.argmax(), raw=True)
-
-    @staticmethod
-    def idxmin(series, window):
-        return series.rolling(window=window).apply(lambda x: (window - 1) - x.argmin(), raw=True)
-
-    @staticmethod
-    def slope(series, window):
-        # 计算每个窗口上的斜率
-        def linear_regression_slope(y):
-            x = np.arange(len(y))
-            A = np.vstack([x, np.ones(len(x))]).T
-            m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-            return m
-
-        return series.rolling(window=window).apply(linear_regression_slope, raw=True)
-
-    @staticmethod
-    def rsquare(series, window):
-        # 对每个窗口计算R平方值
-        def compute_rsquare(y):
-            x = np.arange(len(y))
-            A = np.vstack([x, np.ones(len(x))]).T
-            m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-            y_fit = m * x + c  # 线性拟合值
-            ss_res = np.sum((y - y_fit) ** 2)  # 残差平方和
-            ss_tot = np.sum((y - np.mean(y)) ** 2)  # 总平方和
-            r_squared = 1 - (ss_res / ss_tot)  # R平方计算公式
-            return r_squared
-
-        return series.rolling(window=window).apply(compute_rsquare, raw=True)
-
-    @staticmethod
-    def resi(series, window):
-        # 对每个窗口计算残差
-        def compute_residuals(y):
-            x = np.arange(len(y))
-            A = np.vstack([x, np.ones(len(x))]).T
-            m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-            y_fit = m * x + c  # 线性拟合值
-            residuals = y - y_fit  # 残差
-            return residuals[-1]  # 返回窗口中最后一个残差值
-
-        return series.rolling(window=window).apply(compute_residuals, raw=True)
-
-    @staticmethod
-    def tsrank(series, window):
-        # 计算滚动排名
-        return series.rolling(window=window).apply(lambda x: x.rank().iloc[-1], raw=False)
-
-    @staticmethod
-    def where(condition, true_series, false_series):
-        return pd.Series(np.where(condition, true_series, false_series), index=condition.index)
-
-    @staticmethod
-    def or_operation(condition1, condition2):
-        return pd.Series(np.logical_or(condition1, condition2), index=condition1.index)
-
-    @staticmethod
-    def clip(series, min_value, max_value):
-        return np.clip(series, min_value, max_value)
-
-    @staticmethod
-    def all_quantile(series, percent):
-        return np.nanquantile(series, percent)
-
-    @staticmethod
-    def sma(series, window, weight):
-        weights = np.arange(1, weight + 1)[::-1]  # 生成权重数组，例如当weight=2时，weights为[2, 1]
-        return series.rolling(window=window).apply(lambda x: np.dot(weights[-len(x) :], x[-len(weights) :]) / weights[-len(x) :].sum(), raw=False)
-
-    @staticmethod
-    def wma(series, window):
-        weights = np.arange(1, window + 1)  # 生成权重数组，例如当window=3时，weights为[1, 2, 3]
-        return series.rolling(window=window).apply(lambda x: np.dot(weights, x[-window:]) / weights.sum(), raw=False)
-
-    @staticmethod
-    def decaylinear(series, window):
-        # 生成线性衰减的权重，最旧的数据点权重为1，最新的为window
-        weights = np.arange(1, window + 1)
-        # 计算加权移动平均，每个窗口应用权重并取平均
-        return series.rolling(window=window).apply(lambda x: np.dot(x, weights) / weights.sum(), raw=False)
-
-    @staticmethod
     def tsargmax(series, window):
         return series.rolling(window=window).apply(lambda x: np.argmax(x), raw=True)
 
@@ -166,13 +74,100 @@ class AlphaBaseOperations:
         return series.rolling(window=window).apply(lambda x: np.argmin(x), raw=True)
 
     @staticmethod
+    def tsrank(series, window):
+        return series.rolling(window=window).apply(lambda x: x.rank(pct=True).iloc[-1], raw=False)
+
+    @staticmethod
+    def idxmax(series, window):
+        return series.rolling(window=window).apply(lambda x: (window - 1) - np.argmax(x), raw=True)
+
+    @staticmethod
+    def highday(series, window):
+        return series.rolling(window=window).apply(lambda x: (window - 1) - np.argmax(x), raw=True)
+
+    @staticmethod
+    def idxmin(series, window):
+        return series.rolling(window=window).apply(lambda x: (window - 1) - np.argmin(x), raw=True)
+
+    @staticmethod
+    def lowday(series, window):
+        return series.rolling(window=window).apply(lambda x: (window - 1) - np.argmax(x), raw=True)
+
+    # ------------------------------------------
+    @staticmethod
+    def correlation(series1, series2, window):
+        return series1.rolling(window=window).corr(series2)
+
+    @staticmethod
+    def covariance(series1, series2, window):
+        return series1.rolling(window=window).cov(series2)
+
+    # ------------------------------------------
+    @staticmethod
+    def where(condition, true_series, false_series):
+        return pd.Series(np.where(condition, true_series, false_series), index=condition.index)
+
+    @staticmethod
+    def quantile(series, window, quantile):
+        return series.rolling(window=window).quantile(quantile)
+
+    @staticmethod
+    def clip(series, min_value, max_value):
+        return np.clip(series, min_value, max_value)
+
+    @staticmethod
+    def slope(series, window):
+        def linear_regression_slope(y_values):
+            x_values = np.arange(len(y_values))
+            slope, intercept, r_value, p_value, std_err = linregress(x_values, y_values)
+            return slope
+
+        return series.rolling(window=window).apply(linear_regression_slope, raw=True)
+
+    @staticmethod
+    def rsquare(series, window):
+        def calculate_rsquare(y_values):
+            x_values = np.arange(len(y_values))
+            slope, intercept, r_value, p_value, std_err = linregress(x_values, y_values)
+            return r_value**2
+
+        return series.rolling(window=window).apply(calculate_rsquare, raw=True)
+
+    @staticmethod
+    def resi(series, window):
+        def calculate_residuals(y_values):
+            x_values = np.arange(len(y_values))
+            slope, intercept, r_value, p_value, std_err = linregress(x_values, y_values)
+            line = slope * x_values + intercept
+            return y_values - line
+
+        return series.rolling(window=window).apply(lambda y: calculate_residuals(y)[-1], raw=True)
+
+    @staticmethod
     def signedpower(series, power):
         return np.sign(series) * np.abs(series) ** power
 
     @staticmethod
-    def div(series1, series2, fill_value=np.nan):
-        return series1.div(series2, fill_value=fill_value)
+    def decaylinear(series, window):
+        weights = np.arange(1, window + 1)
+        return series.rolling(window=window).apply(lambda x: np.dot(x, weights) / np.sum(weights), raw=True)
 
+    @staticmethod
+    def sma(series, window, weight=1):
+        weights = np.full(window, weight)
+        return series.rolling(window=window).apply(lambda x: np.dot(x, weights) / np.sum(weights), raw=True)
+
+    @staticmethod
+    def wma(series, window):
+        weights = np.power(0.9, np.arange(window)[::-1])
+        return series.rolling(window=window).apply(lambda x: np.dot(x, weights) / np.sum(weights), raw=True)
+
+    @staticmethod
+    def count(condition_series, window):
+        # 对符合条件的项进行计数
+        return condition_series.rolling(window=window).sum()
+
+    # (以下是不太确定的算子函数)------------------------------------------
     @staticmethod
     def scale(series, factor=1):
         sum_inv = factor / series.sum()
@@ -181,6 +176,21 @@ class AlphaBaseOperations:
     @staticmethod
     def product(series, window):
         return series.rolling(window=window).apply(np.prod, raw=True)
+
+    @staticmethod
+    def sequence(n):
+        return np.arange(1, n + 1)
+
+    @staticmethod
+    def regbeta(series, window):
+        # 定义计算beta的函数，使用线性回归
+        def calculate_beta(x):
+            y = x
+            x = np.arange(1, len(x) + 1)  # 生成时间序列
+            slope, intercept, r_value, p_value, std_err = linregress(x, y)
+            return slope
+
+        return series.rolling(window=window).apply(calculate_beta, raw=True)
 
 
 class AlphaExpressionExcutor:
